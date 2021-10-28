6d9da055fb570bcc7b79cd361c79217aed2ca588
Pasha-Ignatyuk <kiotocloud@gmail.com>
2021-10-28 22:01:06 +0300

    attempt3

--- hrm/settings.py
+++ hrm/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hrm_db',
-        'USER' : 'ceo',
-        'PASSWORD' : 'canton',
+        'USER': 'ceo',
+        'PASSWORD': 'canton',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },
