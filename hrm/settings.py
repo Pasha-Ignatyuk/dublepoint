db8995e10d35a832ac042ae9ce9f0822a56270fb
Pasha-Ignatyuk <kiotocloud@gmail.com>
2021-11-11 00:15:15 +0300

    pylint_django6

--- hrm/settings.py
+++ hrm/settings.py
@@ -15,6 +15,7 @@ import psycopg2.extensions
 from django.apps import AppConfig
 
 AppConfig.default = False
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hrm.settings")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
